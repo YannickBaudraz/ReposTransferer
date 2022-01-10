4323b3020a0c4aa2b5d003e3e4a2272f1b7d9314
Yannick Baudraz <Yannick.BAUDRAZ@cpnv.ch>
2022-01-10 16:59:11 +0100

    Read each lines of a file to transfer repos to the new owner

--- README.md
+++ README.md
@@ -9,8 +9,16 @@ Transfer all repos from a GitHub user to another.
 ## Usage
 
 1. Crate a new token with the permissions to transfer a repo - In GitHub,
-`Settings > Developer Settings > Personal Access Tokens`
+   `Settings > Developer Settings > Personal Access Tokens`
 2. Copy the `.env.example` file to `.env`
-3. Update variables
-4. Run `npm install`
-5. Run `node`
+    1. Update variables
+3. Copy `repos.example.txt` file to `repos.txt`
+    1. Add name of your repos.
+4. Run commands
+    ```shell
+    npm install
+    node transfer.js
+    ```
+6. If nothing is shown in the console, it means that everything went well.
+7. You should receive emails asking to click on a link to confirm the transfer.
+8. Log into the receiving GitHub account in a browser and open links in this browser.
--- transfer.js
+++ transfer.js
@@ -1,16 +1,54 @@
 import {config} from 'dotenv';
 import {Octokit} from '@octokit/core';
+import readline from 'readline';
+import fs from 'fs';
 
-(async () => {
+async function main() {
   config();
 
-  const octokit = new Octokit({
-    auth: process.env.TOKEN,
+  const reposPath = 'repos.txt';
+  const fileStream = fs.createReadStream(reposPath);
+  const rl = readline.createInterface({
+    input: fileStream,
+    crlfDelay: Infinity,
   });
 
-  await octokit.request('POST /repos/{owner}/{repo}/transfer', {
-    owner: process.env.OLD_OWNER,
-    repo: 'repo',
-    new_owner: process.env.NEW_OWNER,
+  for await (const repo of rl) {
+    await transferRepo(
+        repo,
+        process.env.OLD_OWNER,
+        process.env.NEW_OWNER,
+        process.env.TOKEN,
+    );
+  }
+}
+
+/**
+ * Transfer a repo from a GItHub user to another.
+ *
+ * @param {string} repo The name of the repo.
+ * @param {string} owner The old user that give the repo.
+ * @param {string} newOwner The new user that receive the repo.
+ * @param {string} token The token ID to authenticate.
+ *
+ * @return {Promise<void>}
+ */
+async function transferRepo(repo, owner, newOwner, token) {
+  const octokit = new Octokit({auth: token});
+  const route = 'POST /repos/{owner}/{repo}/transfer';
+
+  const response = await octokit.request(route, {
+    owner,
+    repo,
+    new_owner: newOwner,
   });
-})();
+
+  console.info(
+      'Status [' + response.status + '] : ' +
+      'Initialized [' + repo + '] transfer ' +
+      'from [' + owner + '] ' +
+      'to [' + newOwner + ']',
+  );
+}
+
+await main();
